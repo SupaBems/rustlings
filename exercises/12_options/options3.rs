20a9af6bcf252c53fa3ff932a1a2e86fec21a980
Anaël KONZATE-KPONY <144048812+SupaBems@users.noreply.github.com>
2024-06-06 11:17:57 +0200

    Update options3.rs

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -13,9 +13,10 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+    y; // Comme nous avons utilisé un match &y, ce y ne causera pas de problème.
 }
+
